a5c51f81a0e5f0c0e803b6661159afd627bbb03d
agentzh (章亦春) <agentzh@gmail.com>
2010-04-02 15:39:17 +0800

    applied the patch from Sergey A. Osokin to work with nginx 0.8.35.

--- src/ngx_http_echo_timer.c
+++ src/ngx_http_echo_timer.c
@@ -23,7 +23,14 @@ ngx_http_echo_timer_elapsed_variable(ngx_http_request_t *r,
         ctx->timer_begin.msec = (ngx_msec_t) r->start_msec;
     }
 
-    ngx_time_update(0, 0); /* force the ngx timer to update */
+    /* force the ngx timer to update */
+
+#if defined nginx_version && nginx_version >= 8035
+    ngx_time_update();
+#else
+    ngx_time_update(0, 0);
+#endif
+
     tp = ngx_timeofday();
 
     dd("old sec msec: %ld %d\n", ctx->timer_begin.sec, ctx->timer_begin.msec);
@@ -52,7 +59,14 @@ ngx_http_echo_exec_echo_reset_timer(ngx_http_request_t *r,
         ngx_http_echo_ctx_t *ctx)
 {
     dd("Exec timer...");
-    ngx_time_update(0, 0); /* force the ngx timer to update */
+
+    /* force the ngx timer to update */
+
+#if defined nginx_version && nginx_version >= 8035
+    ngx_time_update();
+#else
+    ngx_time_update(0, 0);
+#endif
 
     ctx->timer_begin = *ngx_timeofday();
     return NGX_OK;
