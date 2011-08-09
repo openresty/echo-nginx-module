b825665d69f00113f42045e6428f59a452154e6a
agentzh (章亦春) <agentzh@gmail.com>
2011-08-09 14:44:30 +0800

    confirmed that we work with nginx 1.0.5.

--- util/build.sh
+++ util/build.sh
@@ -4,40 +4,10 @@
 
 root=`pwd`
 version=$1
-#opts=$2
+force=$2
 home=~
-target=$root/work/nginx
 
-if [ ! -d ./buildroot ]; then
-    mkdir ./buildroot || exit 1
-fi
-
-cd buildroot || exit 1
-
-if [ ! -s "nginx-$version.tar.gz" ]; then
-    if [ -f ~/work/nginx-$version.tar.gz ]; then
-        cp ~/work/nginx-$version.tar.gz ./ || exit 1
-    else
-        wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz || exit 1
-        cp nginx-$version.tar.gz ~/work/
-    fi
-
-    tar -xzvf nginx-$version.tar.gz || exit 1
-    cp $root/../no-pool-nginx/nginx-$version-no_pool.patch ./ || exit 1
-    patch -p0 < nginx-$version-no_pool.patch || exit 1 || exit 1
-fi
-
-#tar -xzvf nginx-$version.tar.gz || exit 1
-#cp $root/../no-pool-nginx/nginx-$version-no_pool.patch ./ || exit 1
-#patch -p0 < nginx-$version-no_pool.patch || exit 1 || exit 1
-#patch -p0 < ~/work/nginx-$version-rewrite_phase_fix.patch || exit 1
-
-cd nginx-$version/
-
-if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile \
-        || "$root/config" -nt Makefile
-        || "$root/util/build.sh" -nt Makefile ]]; then
-    ./configure --prefix=$target \
+ngx-build $force $version \
             --with-cc-opt="-DDEBUG_MALLOC" \
             --with-http_stub_status_module \
             --without-mail_pop3_module \
@@ -66,14 +36,4 @@ if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile \
           #--add-module=$root/../eval-nginx-module \
           #--add-module=/home/agentz/work/nginx_eval_module-1.0.1 \
   #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
-
-fi
-if [ -f $target/sbin/nginx ]; then
-    rm -f $target/sbin/nginx
-fi
-if [ -f $target/logs/nginx.pid ]; then
-    kill `cat $target/logs/nginx.pid`
-fi
-make -j3
-make install
 
